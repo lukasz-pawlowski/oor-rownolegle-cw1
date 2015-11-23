65d8c1148bcd4523158c3b627137fa3d79513041
Lukasz Pawlowski <lukp12@gmail.com>
2015-11-23 17:15:10 +0100

    initial commit

---
+++ projekt1-wielowatkowosc/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace projekt1_wielowatkowosc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFib = new System.Windows.Forms.Label();
+            this.labelHash = new System.Windows.Forms.Label();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFib
+            // 
+            this.labelFib.AutoSize = true;
+            this.labelFib.Location = new System.Drawing.Point(29, 13);
+            this.labelFib.Name = "labelFib";
+            this.labelFib.Size = new System.Drawing.Size(35, 13);
+            this.labelFib.TabIndex = 0;
+            this.labelFib.Text = "label1";
+            // 
+            // labelHash
+            // 
+            this.labelHash.AutoSize = true;
+            this.labelHash.Location = new System.Drawing.Point(32, 236);
+            this.labelHash.Name = "labelHash";
+            this.labelHash.Size = new System.Drawing.Size(35, 13);
+            this.labelHash.TabIndex = 1;
+            this.labelHash.Text = "label1";
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.Location = new System.Drawing.Point(32, 51);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(200, 160);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(145, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.labelHash);
+            this.Controls.Add(this.labelFib);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFib;
+        private System.Windows.Forms.Label labelHash;
+        public System.Windows.Forms.RichTextBox richTextBox;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ projekt1-wielowatkowosc/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace projekt1_wielowatkowosc
+{
+    public partial class Form1 : Form
+    {
+
+        // This delegate enables asynchronous calls for setting
+        // the text property on a TextBox control.
+        delegate void SetTextCallback(string text);
+        delegate void GetTextCallback();
+        delegate void SetLabelHashCallback(string text);
+        delegate string GetLabelHashCallback();
+        delegate void SetLabelFibCallback(string text);
+        delegate string GetLabelFibCallback();
+
+
+        Thread rwThread, fibThread, hashThread;
+        String richTextString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            //threads
+            rwThread = new Thread(readWrite);
+            rwThread.Start();
+
+            fibThread = new Thread(fib);
+            fibThread.Start();
+
+            hashThread = new Thread(calculateHash);
+            hashThread.Start();
+            
+        }
+
+        private void readWrite()
+        {
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1001);
+                this.GetText();
+                string s = richTextString;
+                //zapis
+                byte[] data = Encoding.UTF8.GetBytes(s);
+                string path = @"c:\x.txt";
+                FileStream fs = new System.IO.FileStream(path, FileMode.Create);
+                fs.Write(data, 0, data.Length);
+                fs.Close();
+
+                //odczyt
+                //byte[] data;
+                //    string path = @"c:\x.txt";
+                if (File.Exists(path))
+                {
+                    FileStream fso = new System.IO.FileStream(path, FileMode.Open);
+                    data = new byte[fso.Length];
+                    fso.Position = 0;
+                    fso.Read(data, 0, (int)fso.Length);
+                    fso.Close();
+                }
+            }
+        }
+
+        private void fib()
+        {
+            //throw new NotImplementedException();
+            int prev = 1;
+            int next = 1;
+            int sum = 0;
+
+            while(true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                sum = prev + next;
+                prev = next;
+                next = sum;
+                this.SetLabelFib(sum.ToString());
+
+            }
+
+        }
+        
+
+        //calculate hash of all data
+        private void calculateHash()
+        {
+            while (true)
+            {
+                this.GetText();
+                String s = richTextString;
+
+                System.Threading.Thread.Sleep(1000);
+                int h = s.GetHashCode();
+                this.SetLabelHash(h.ToString());
+            }
+        }
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.richTextBox.Text = text;
+            }
+        }
+        private void GetText()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox.InvokeRequired)
+            {
+                GetTextCallback d = new GetTextCallback(GetText);
+                this.Invoke(d, new object[] {  });
+               
+            }
+            else
+            {
+                richTextString = this.richTextBox.Text;
+
+            }
+        }
+        private void SetLabelFib(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox.InvokeRequired)
+            {
+                SetLabelFibCallback d = new SetLabelFibCallback(SetLabelFib);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.labelFib.Text = text;
+            }
+        }
+        private string GetLabelFib()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox.InvokeRequired)
+            {
+                GetLabelFibCallback d = new GetLabelFibCallback(GetLabelFib);
+                this.Invoke(d, new object[] { });
+                return "empty";
+            }
+            else
+            {
+                return this.labelFib.Text;
+
+            }
+        }
+        private void SetLabelHash(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox.InvokeRequired)
+            {
+                SetLabelHashCallback d = new SetLabelHashCallback(SetLabelHash);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.labelHash.Text = text;
+            }
+        }
+        private string GetLabelHash()
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.richTextBox.InvokeRequired)
+            {
+                GetLabelHashCallback d = new GetLabelHashCallback(GetLabelHash);
+                this.Invoke(d, new object[] { });
+                return "empty";
+            }
+            else
+            {
+                return this.labelHash.Text;
+
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            rwThread.Abort();
+            fibThread.Abort();
+            hashThread.Abort();
+        }
+    }
+}
